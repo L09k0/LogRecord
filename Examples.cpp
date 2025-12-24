ae23aebe3ca12ff7641f20f5e099ad80518520b8
Alexandr L <aghlwiki.logko@gmail.com>
2025-12-24 05:36:39 +0300

    v1.1

--- Examples.cpp
+++ Examples.cpp
@@ -1,16 +1,25 @@
-#include <locale>
-#include <iostream>
-
 #include "LogRecord.h"
 
 int main()
 {
-	setlocale(NULL, "Russian");
+	// Test Status
+	INFO_MSG("String", true);
+	WARNING_MSG("String", true);
+	ERROR_MSG("String", true);
+	FATALERROR_MSG("String", true);
+	REGISTER_MSG("String", true);
+	INITIALIZATION_MSG("String", true);
+	CLIENT_MSG("String", true);
+	SERVER_MSG("String", true);
+	EXCEPTION_MSG("String", true);
 
-	LogRecord::GetInstance()->msg("std::string", LogRecord::StatusMsg::_INFO, true);
-	LogRecord::GetInstance()->msg(L"std::wstring", LogRecord::StatusMsg::_INFO, true);
-	LogRecord::GetInstance()->msg(1, LogRecord::StatusMsg::_INFO, true);
-	LogRecord::GetInstance()->msg(1.1, LogRecord::StatusMsg::_INFO, true);
+	// Test type
+	INFO_MSG(L"WString", true);
+	INFO_MSG(12345, true);
+	INFO_MSG(3.14, true);
+
+	// Test visible console
+	INFO_MSG("String", false);
 
 	system("pause");
 	return 0;
